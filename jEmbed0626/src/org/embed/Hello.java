8da8465e225c6e1311fc6b1e27004f74adf41934
user <user@DESKTOP-0RTCS98>
2025-09-05 09:05:50 +0900

    09/04

--- jEmbed0626/src/org/embed/Hello.java
+++ jEmbed0626/src/org/embed/Hello.java
@@ -4,7 +4,16 @@ public class Hello {
 
 	public static void main(String args[]) {
 	
+		String i = "10";
+		String age = "10";
+		
 		System.out.println("Hello, World");
+		
+		System.out.println(Integer.parseInt(i+1));
+		System.out.println(Integer.parseInt(i)+1);
+		
+		System.out.println("UPDATE " + " SET age=" + (Integer.parseInt(age) + 1) + " WHERE name='"  + "'");
+		System.out.println("UPDATE " + " SET age=" + (age + 1) + " WHERE name='"  + "'");
 	
 	}
 }
